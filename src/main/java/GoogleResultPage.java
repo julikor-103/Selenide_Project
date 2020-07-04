e975815789c589e0e0f250ebf9c4f3e8cc300c21
julikor103 <julikor103@gmail.com>
2020-07-04 17:05:31 +0300

    HW_22

---
+++ src/main/java/GoogleResultPage.java
@@ -0,0 +1,11 @@
+import com.codeborne.selenide.ElementsCollection;
+
+import static com.codeborne.selenide.Selenide.$$;
+
+public class GoogleResultPage {
+
+  public ElementsCollection results() {
+      return $$("ires li.g");
+
+}
+}
